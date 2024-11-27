d9fbc2cf15fe11761e60a4c7785646a3dd78d70d
truongngo99 <truong.ngo@executionlab.asia>
2024-11-27 10:23:37 +0700

    feat: change maven to gradle

--- src/main/java/com/example/demoSpring/DemoSpringApplication.java
+++ src/main/java/com/example/demoSpring/DemoSpringApplication.java
@@ -1,13 +1,9 @@
 package com.example.demoSpring;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DemoSpringApplication {
 
 	public static void main(String[] args) {
